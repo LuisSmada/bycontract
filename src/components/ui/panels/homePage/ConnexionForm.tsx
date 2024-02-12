60e6ca7a6e714b79f85f3b11f1e083dd61735c77
LuisSmada <ayoadams@outlook.fr>
2024-02-12 23:32:15 +0100

    feature: add another things on login form

--- src/components/ui/panels/homePage/ConnexionForm.tsx
+++ src/components/ui/panels/homePage/ConnexionForm.tsx
@@ -5,6 +5,7 @@ import Logo from "../../../../assets/icons/BYC.svg";
 export const ConnexionForm = () => {
   const [userLogin, setUserLogin] = useState<string>("");
   const [userPassword, setUserPassword] = useState<string>("");
+  const [isChecked, setIsChecked] = useState<boolean>(false);
 
   return (
     <Container>
@@ -30,7 +31,12 @@ export const ConnexionForm = () => {
             onChange={(e) => setUserPassword(e.target.value)}
           />
           <RememberMeWrapper>
-            <CheckBox type="checkbox" />
+            <CheckBox
+              type="checkbox"
+              checked={isChecked}
+              onChange={(e) => setIsChecked(e.target.checked)}
+              $isChecked={isChecked}
+            />
             <Label>Se souvenir de moi</Label>
           </RememberMeWrapper>
           <Button>Connexion</Button>
@@ -88,29 +94,38 @@ const Label = styled.div`
   font-size: ${(props) => props.theme.textSize.normalText};
   font-weight: 600;
   color: ${(props) => props.theme.colors.mainText};
-  margin-bottom: 0.5rem;
+  margin-bottom: 0.3rem;
 `;
 
 const RememberMeWrapper = styled.div`
-  display: flex;
-  align-items: center;
+  margin-bottom: 0.5rem;
+  display: grid;
+  grid-template-columns: 1em auto;
+  gap: 0.5em;
 `;
 
 const InputForm = styled.input`
   width: 402px;
-  height: 38px;
-  border: ${(props) => `2px solid ${props.theme.colors.disabled}`};
+  height: 30px;
+  border: ${(props) => `1px solid ${props.theme.colors.disabled}`};
   border-radius: 5px;
   margin-bottom: 10px;
   outline: none;
   &:focus {
-    border: ${(props) => `2px solid ${props.theme.colors.main}`};
+    border: ${(props) => `1px solid ${props.theme.colors.main}`};
   }
 `;
 
-const CheckBox = styled.input`
-  width: 20px;
-  height: 20px;
+interface ICheckBox {
+  $isChecked: boolean;
+}
+
+const CheckBox = styled.input<ICheckBox>`
+  border-radius: 5px;
+  border: none;
+  width: 15px;
+  height: 15px;
+  background-color: ${(props) => props.$isChecked && `#000`};
 `;
 
 const Button = styled.button`
@@ -126,5 +141,20 @@ const Button = styled.button`
 `;
 
 const ButtonLanguage = styled.button`
-  width: auto;
+  width: 6rem;
+  height: 30px;
+  background-color: white;
+  border: ${(props) => `1px solid ${props.theme.colors.borderField}`};
+  border-radius: 5px;
+  color: ${(props) => props.theme.colors.disabled};
+  font-size: 13px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+  cursor: pointer;
+  &:focus {
+    border: ${(props) => `1px solid ${props.theme.colors.main}`};
+    color: ${(props) => props.theme.colors.main};
+  }
 `;
